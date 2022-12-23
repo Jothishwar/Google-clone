2cda5c723b0cf6d56e634057744b1fe972b54443
jothishwar <jothishwarvvr@gmail.com>
2022-12-23 18:00:42 +0530

    styled home,made input functional

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Search.css';
 import SearchIcon from '@mui/icons-material/Search';
 import MicIcon from '@mui/icons-material/Mic';
+import Button from '@mui/material/Button';
 
 function Search() {
+
+	const [input,setInput] = useState("");
+
+	const search = e => {
+		e.preventDefault();
+		console.log('input >>', input);
+	}
+
 	return (
-		<div className="search">
+		<form className="search">
 			<div className="search_input">
 				<SearchIcon className="search_inputIcon" />
-				<input />
+				<input value={input} onChange={e=>setInput(e.target.value)}/>
 				<MicIcon />
 			</div>
-		</div>
+			<div className="search_buttons">
+				<Button type="submit" onClick={search} variant="outlined">Google Search</Button>
+				<Button variant="outlined">I'm Feeling Lucky </Button>
+			</div>
+		</form>
 	)
 }
 
