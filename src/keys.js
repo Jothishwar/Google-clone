52ce48a1f2699d9e844ff7110445021c5fe2a94d
jothishwar <jothishwarvvr@gmail.com>
2022-12-24 13:02:23 +0530

    custom search api and fetch data

---
+++ src/keys.js
@@ -0,0 +1,3 @@
+export const API_KEY = "AIzaSyCo7h45NZg_VEp-hjdMtHtAZQ1GLTe-XlM";
+
+export default API_KEY;
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import './SearchPage.css';
+import { useStateValue } from "../StateProvider";
+import useGoogleSearch from "../useGoogleSearch";
 
 function SearchPage() {
+
+	const [{term},dispatch] = useStateValue();
+
+	const { data } = useGoogleSearch(term);
+
+	console.log(data);
 	return (
 		<div className="searchpage">
-			
+			<div className="searchpage_header">
+				<h1>{term}</h1>
+			</div>
+			<div className="searchpage_results">
+
+			</div>
 		</div>
 	)
 }
